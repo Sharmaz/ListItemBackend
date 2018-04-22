8df52415f3399475cd2a55f2331badc81ecfc499
Ivan Robles <irae45@gmail.com>
2018-04-22 18:07:27 -0500

    Validamos los casos en los que se pudiera aplicar alguna acción del API si no existen las listas o items referidos a un ID

--- api/controllers/items.js
+++ api/controllers/items.js
@@ -10,47 +10,112 @@ const List = require('../models/lists');
  * findById()  Muestra un item por ID
  * update()    Actualiza un campo de un item
  * remove()    Elimina un item
+ * Validamos si existe la lista o el item por ID antes de obtener, crear, modificar o eliminar.
  * Respondemos retornando JSON con el objeto buscado, creado, modificado o eliminado.
  */
 const ItemsController = {
   getItems: async (req, res) => {
+    
+    // Mostramos todos los items seleccionando el ID y el Nombre
     const items = await Item.find({}).select('_id name');
     res.status(200).json({items});
   },
   createItems: async (req, res) => {
-    const list = await List.findById(req.body.listId)
+    const list = await List.findById(req.body.listId);
     const newItem = await new Item({
       list: req.body.listId,
       name: req.body.name
     });
-    list.items.push(newItem);
-    list.save();
-    const item = await newItem.save();
-    res.status(201).json(item);
+    
+    // Si no existe la lista mandamos Error 404
+    if (!list) {
+      res.status(404).json({
+        message: 'Not entry found provided by that ID'
+      });
+    }
+    else {
+
+      // Si existe la lista agregamos el item nuevo al arreglo de items de la lista
+      list.items.push(newItem);
+      
+      // Guardamos la lista con los cambios en la base de datos
+      list.save();
+      
+      // Guardamos el item nuevo en la base de datos
+      const item = await newItem.save();
+
+      // Respondemos con los datos ID y nombre del nuevo item
+      res.status(201).json({
+        message: 'Item Created!!',
+          _id: item._id,
+          name: item.name
+      });
+    }
   },
   getItemsById: async (req, res) => {
-    const itemId = req.params.itemId;
+    
+    /* Asignación por destructuring, es lo mismo que:
+     * const itemId = req.params.itemId;
+     */
+    const { itemId } = req.params;
     const itemResult = await Item.findById(itemId).select('_id name');
-    res.status(200).json(itemResult);
+    
+    // Si no existe el item con cierto ID mandamos Error 404
+    if (!itemResult) {
+      res.status(404).json({
+        message: 'Not entry found provided by that ID'
+      });
+    }
+    else {
+      
+      // Respondemos con el resultado del item
+      res.status(200).json(itemResult);
+    }
   },
   updateItems: async (req, res) => {
-    const itemId = req.params.itemId;
-    const itemUpdated = await Item.update(
-      {_id: itemId}, 
-      {$set: {name: req.body.newName}});
-    res.status(200).json({
-      message: "Item Updated!!",
-      itemId,
-      name: req.body.newName
-    });
+    const { itemId } = req.params;
+    const itemResult = await Item.findById(itemId);
+    
+    // Si no existe el item con cierto ID mandamos Error 404
+    if (!itemResult) {
+      res.status(404).json({
+        message: 'Not entry found provided by that ID'
+      });
+    }
+    else {
+
+      // Actualizamos el item pasandole a update() el ID, le decimos que cambie name
+      const itemUpdated = await Item.update(
+        {_id: itemId}, 
+        {$set: {name: req.body.newName}});
+      
+      // Respondemos con los datos del item actualizado
+      res.status(200).json({
+        message: 'Item Updated!!',
+        itemId,
+        name: req.body.newName
+      });
+    }
   },
   deleteItems: async (req, res) => {
-    const itemId = req.params.itemId;
-    const itemRemoved = await Item.remove({_id: itemId});
-    res.status(200).json({
-      message: "List Deleted!!",
-      itemId
-    });
+    const { itemId } = req.params;
+    const itemResult = await Item.findById(itemId);
+    
+    // Si no existe el item con cierto ID, mandamos Error 404
+    if (!itemResult) {
+      res.status(404).json({
+        message: 'Not entry found provided by that ID'
+      });
+    }
+    else {
+
+      // Si existe el item, lo eliminamos por ID con remove()
+      const itemRemoved = await Item.remove({_id: itemId});
+      res.status(200).json({
+        message: 'List Deleted!!',
+        itemId
+      });
+    }
   }
 };
 
--- api/controllers/lists.js
+++ api/controllers/lists.js
@@ -3,48 +3,104 @@
 
 // Dependencias
 const List = require('../models/lists');
-const debug = require('debug')('listitem:controllers-list')
 
 /* Utilizamos los metodos de mongoose para manipular la base de datos de mongodb
  * find()      Muestra todas las listas
  * findById()  Muestra una lista por ID
  * update()    Actualiza un campo de la lista
  * remove()    Elimina una lista
+ * Validamos si existe la lista por ID antes de obtener, modificar o eliminar.
  * Respondemos retornando JSON con el objeto buscado, creado, modificado o eliminado.
  */
 const ListsController = {
   getLists: async (req, res) => {
+
+    /* Mostramos todas las listas seleccionando el ID y el Nombre
+     * Con populate() hacemos algo similar a un join en SQL, en este caso con los Items
+     * Dentro del populate seleccionamoes el ID y Nombre del Item
+     */
     const lists = await List.find({}).select('_id name').populate('items', '_id name');
     res.status(200).json({lists});
   },
   createLists: async (req, res) => {
     const newlist = await new List(req.body);
+
+    // Guardamos la lista en la base de datos con save()
     const list = await newlist.save();
-    res.status(201).json(list);
+
+    // Respondemos con los datos ID y name de la lista creada
+    res.status(201).json({
+      message: 'Created List!!',
+      createList: {
+        _id: list._id,
+        name: list.name
+      }
+    });
   },
   getListsById: async (req, res) => {
-    const listId = req.params.listId;
+
+    /* Asignación por destructuring, es lo mismo que:
+     * const listId = req.params.listId;
+     */
+    const { listId } = req.params;
     const listResult = await List.findById(listId).select('_id name');
-    res.status(200).json(listResult);
+    
+    // Si no existe la lista por ID, mandamos error 404
+    if (!listResult) {
+      res.status(404).json({
+        message: 'Not entry found provided by that ID'
+      });
+    }
+    
+    // Mandamos como respuesta el resultado de la busqueda y selección
+    else {
+      res.status(200).json(listResult);
+    }
   },
   updateLists: async (req, res) => {
-    const listId = req.params.listId;
-    const listUpdated = await List.update(
-      {_id: listId}, 
-      {$set: {name: req.body.newName}});
-    res.status(200).json({
-      message: "List Updated!!",
-      listId,
-      name: req.body.newName
-    });
+    const { listId } = req.params;
+    const listResult = await List.findById(listId);
+    
+    // Si no existe la lista por ID mandamos error 404
+    if (!listResult) {
+      res.status(404).json({
+        message: 'Not entry found provided by that ID'
+      });
+    }
+    else {
+      
+      // Actualizamos la lista pasandole a update() el ID, le decimos que cambie name
+      const listUpdated = await List.update(
+        {_id: listId}, 
+        {$set: {name: req.body.newName}});
+      
+      // Respondemos con los datos de la lista actualizada
+      res.status(200).json({
+        message: 'List Updated!!',
+        listId,
+        name: req.body.newName
+      });
+    }
   },
   deleteLists: async (req, res) => {
-    const listId = req.params.listId;
-    const listRemoved = await List.remove({_id: listId});
-    res.status(200).json({
-      message: "List Deleted!!",
-      listId
-    });
+    const { listId } = req.params;
+    const listResult = await List.findById(listId);
+    
+    // Si no existe la lista por ID mandamos error 404
+    if (!listResult) {
+      res.status(404).json({
+        message: 'Not entry found provided by that ID'
+      });
+    } 
+    else {
+      
+      // Eliminamos la lista con remove()
+      const listRemoved = await List.remove({_id: listId});
+      res.status(200).json({
+        message: 'List Deleted!!',
+        listId
+      });
+    }
   }
 };
 
